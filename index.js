880ce5db5e24986dadd6f3424af4e67c9c61c217
samikhya <mishrasamikhya54@gmail.com>
2023-10-21 20:46:05 +0530

    caps X

--- index.js
+++ index.js
@@ -1,70 +1,82 @@
+var screen=document.querySelector('#screen');
+    var btn=document.querySelectorAll('.btn');
 
-var screen = document.querySelector('#screen');
-var btn = document.querySelectorAll('.btn');
+    /*============ For getting the value of btn, Here we use for loop ============*/
+    for(item of btn)
+    {
+        item.addEventListener('click',(e)=>{
+            btntext=e.target.innerText;
+            console.log('Button Text is', btntext);
 
-/*============ For getting the value of btn, Here we use for loop ============*/
-for (item of btn) {
-    item.addEventListener('click', (e) => {
-        btntext = e.target.innerText;
-        console.log('Button Text is ',btntext)
+            if(btntext =='X')
+            {
+                btntext= '*';
+            }
 
-        if (btntext == '×') {
-            btntext = '*';
-        }
-
-        if (btntext == '÷') {
-            btntext = '/';
-        }
-        screen.value += btntext;
-    });
-}
-
-function sin() {
-    screen.value = Math.sin(screen.value);
-}
-
-function cos() {
-    screen.value = Math.cos(screen.value);
+            if(btntext=='÷')
+            {
+                btntext='/';
+            }
+            screen.value+=btntext;
+        });
     }
 
-    function tan() {
-        screen.value = Math.tan(screen.value);
+    function sin()
+    {
+        screen.value=Math.sin(screen.value);
     }
 
-    function pow() {
-        screen.value = Math.pow(screen.value, 2);
+    function cos()
+    {
+        screen.value=Math.cos(screen.value);
     }
 
-    function sqrt() {
-        screen.value = Math.sqrt(screen.value, 2);
+    function tan()
+    {
+        screen.value=Math.tan(screen.value);
     }
 
-    function log() {
-        screen.value = Math.log(screen.value);
+    function pow()
+    {
+        screen.value=Math.pow(screen.value,2);
     }
 
-    function pi() {
-        screen.value = 3.14159265359;
+    function sqrt()
+    {
+        screen.value=Math.sqrt(screen.value,2);
     }
 
-    function e() {
-        screen.value = 2.71828182846;
+    function log()
+    {
+        screen.value=Math.log(screen.value);
     }
 
-    function fact() {
+    function pi()
+    {
+        screen.value= 3.14159265359;
+    }
+
+    function e()
+    {
+        screen.value=2.71828182846;
+    }
+
+    function fact()
+    {
         var i, num, f;
-        f = 1
-        num = screen.value;
-        for (i = 1; i <= num; i++) {
-            f = f * i;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+            f=f*i;
         }
 
-        i = i - 1;
+        i=i-1;
 
-        screen.value = f;
+        screen.value=f;
     }
 
-    function backspc() {
-        screen.value = screen.value.substr(0, screen.value.length - 1);
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
     }
-</script>
