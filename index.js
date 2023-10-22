7030db88f1378324a3070aeed4182e3a170f14d7
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2023-10-22 10:55:37 +0530

    Update index.js
    I've cleaned up and added comments to the JavaScript code to improve readability

--- index.js
+++ index.js
@@ -1,82 +1,76 @@
-var screen=document.querySelector('#screen');
-    var btn=document.querySelectorAll('.btn');
+// Get the screen and all buttons
+var screen = document.querySelector('#screen');
+var buttons = document.querySelectorAll('.btn');
 
-    /*============ For getting the value of btn, Here we use for loop ============*/
-    for(item of btn)
-    {
-        item.addEventListener('click',(e)=>{
-            btntext=e.target.innerText;
-            console.log('Button Text is', btntext);
+// Add event listeners to buttons
+for (var button of buttons) {
+    button.addEventListener('click', function (e) {
+        var buttonText = e.target.innerText;
+        console.log('Button Text is', buttonText);
 
-            if(btntext =='X')
-            {
-                btntext= '*';
-            }
-
-            if(btntext=='÷')
-            {
-                btntext='/';
-            }
-            screen.value+=btntext;
-        });
-    }
-
-    function sin()
-    {
-        screen.value=Math.sin(screen.value);
-    }
-
-    function cos()
-    {
-        screen.value=Math.cos(screen.value);
-    }
-
-    function tan()
-    {
-        screen.value=Math.tan(screen.value);
-    }
-
-    function pow()
-    {
-        screen.value=Math.pow(screen.value,2);
-    }
-
-    function sqrt()
-    {
-        screen.value=Math.sqrt(screen.value,2);
-    }
-
-    function log()
-    {
-        screen.value=Math.log(screen.value);
-    }
-
-    function pi()
-    {
-        screen.value= 3.14159265359;
-    }
-
-    function e()
-    {
-        screen.value=2.71828182846;
-    }
-
-    function fact()
-    {
-        var i, num, f;
-        f=1
-        num=screen.value;
-        for(i=1; i<=num; i++)
-        {
-            f=f*i;
+        if (buttonText === 'X') {
+            buttonText = '*';
         }
 
-        i=i-1;
+        if (buttonText === '÷') {
+            buttonText = '/';
+        }
 
-        screen.value=f;
+        // Append the button's text to the screen
+        screen.value += buttonText;
+    });
+}
+
+// Perform trigonometric calculations
+function sin() {
+    screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+    screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+    screen.value = Math.tan(screen.value);
+}
+
+// Calculate power and square root
+function pow() {
+    screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+    screen.value = Math.sqrt(screen.value);
+}
+
+// Calculate the natural logarithm
+function log() {
+    screen.value = Math.log(screen.value);
+}
+
+// Set the value of pi and e
+function pi() {
+    screen.value = Math.PI;
+}
+
+function e() {
+    screen.value = Math.E;
+}
+
+// Calculate factorial
+function fact() {
+    var i, num, f;
+    f = 1;
+    num = parseFloat(screen.value);
+
+    for (i = 1; i <= num; i++) {
+        f = f * i;
     }
 
-    function backspc()
-    {
-        screen.value=screen.value.substr(0,screen.value.length-1);
-    }
+    screen.value = f;
+}
+
+// Remove the last character from the screen
+function backspc() {
+    screen.value = screen.value.slice(0, -1);
+}
